29e9672d648ab63e1976246fcaa4cc1ba4441a55
Cobey Lusinger <cobeylusinger@gmail.com>
2017-04-23 22:51:14 -0400

    22:51 23/04/2017

--- inger/studio/blog/blogscripts.js
+++ inger/studio/blog/blogscripts.js
@@ -62,44 +62,146 @@ $(document).ready(function(){
       $("a.about").click(function(e) {
           e.preventDefault();
 
+          $(".postnavigation").css("display","none");
           $("#portal").attr("src", $(this).attr("href"));
       })
 
       $("a.blog").click(function(e) {
           e.preventDefault();
 
+          $(".postnavigation").css("display","block");
           $("#portal").attr("src", $(this).attr("href"));
       })
 
       $("a.archive").click(function(e) {
           e.preventDefault();
 
+          $(".postnavigation").css("display","none");
           $("#portal").attr("src", $(this).attr("href"));
       })
 
+      $("a.black").click(function(e) {
+
+          $(".postnavigation").css("display","none");
+      })
+
+      /*NAVIGATION HOVER*/
+
+      $('.post1').mousemove(function(e) {
+            console.log("mousemove works");
+
+          var $img = $("#" + $(this).data('image-id'))
+          $img.show();
+          $img.offset({
+              top: e.pageY - 15,
+              left: e.pageX + 20
+          });
+      }).mouseleave(function() {
+          $img = $("#" + $(this).data('image-id'))
+          $img.hide();
+      });
+
+      $('.post2').mousemove(function(e) {
+            console.log("mousemove works");
+
+          var $img = $("#" + $(this).data('image-id'))
+          $img.show();
+          $img.offset({
+              top: e.pageY - 15,
+              left: e.pageX + 20
+          });
+      }).mouseleave(function() {
+          $img = $("#" + $(this).data('image-id'))
+          $img.hide();
+      });
+
+      $('.post3').mousemove(function(e) {
+            console.log("mousemove works");
+
+          var $img = $("#" + $(this).data('image-id'))
+          $img.show();
+          $img.offset({
+              top: e.pageY - 15,
+              left: e.pageX + 20
+          });
+      }).mouseleave(function() {
+          $img = $("#" + $(this).data('image-id'))
+          $img.hide();
+      });
+
+      $('.post4').mousemove(function(e) {
+            console.log("mousemove works");
+
+          var $img = $("#" + $(this).data('image-id'))
+          $img.show();
+          $img.offset({
+              top: e.pageY - 15,
+              left: e.pageX + 20
+          });
+      }).mouseleave(function() {
+          $img = $("#" + $(this).data('image-id'))
+          $img.hide();
+      });
+
+      $('.post5').mousemove(function(e) {
+            console.log("mousemove works");
+
+          var $img = $("#" + $(this).data('image-id'))
+          $img.show();
+          $img.offset({
+              top: e.pageY - 15,
+              left: e.pageX + 20
+          });
+      }).mouseleave(function() {
+          $img = $("#" + $(this).data('image-id'))
+          $img.hide();
+      });
+
+      $('.post6').mousemove(function(e) {
+            console.log("mousemove works");
+
+          var $img = $("#" + $(this).data('image-id'))
+          $img.show();
+          $img.offset({
+              top: e.pageY - 15,
+              left: e.pageX + 20
+          });
+      }).mouseleave(function() {
+          $img = $("#" + $(this).data('image-id'))
+          $img.hide();
+      });
+
+      $('.post7').mousemove(function(e) {
+            console.log("mousemove works");
+
+          var $img = $("#" + $(this).data('image-id'))
+          $img.show();
+          $img.offset({
+              top: e.pageY - 15,
+              left: e.pageX + 20
+          });
+      }).mouseleave(function() {
+          $img = $("#" + $(this).data('image-id'))
+          $img.hide();
+      });
+
+      $('.post8').mousemove(function(e) {
+            console.log("mousemove works");
+
+          var $img = $("#" + $(this).data('image-id'))
+          $img.show();
+          $img.offset({
+              top: e.pageY - 15,
+              left: e.pageX + 20
+          });
+      }).mouseleave(function() {
+          $img = $("#" + $(this).data('image-id'))
+          $img.hide();
+      });
 
   });
 
-  $(document).ready(function(){
-    console.log("Hey again.");
 
-
-
-    $('.orlandobook').mousemove(function(e) {
-          console.log("mousemove works");
-
-        var $img = $("#" + $(this).data('image-id'))
-        $img.show();
-        $img.offset({
-            top: e.pageY + 20,
-            left: e.pageX + 20
-        });
-    }).mouseleave(function() {
-        $img = $("#" + $(this).data('image-id'))
-        $img.hide();
-    });
-
-  });  
 
 
 
