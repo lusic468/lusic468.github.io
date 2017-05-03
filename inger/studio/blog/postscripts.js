005f8fc5543e001276f2906ac39f1ba63b2a8386
Cobey Lusinger <cobeylusinger@gmail.com>
2017-05-02 21:36:41 -0400

    21:36 02/05/2017

--- inger/studio/blog/postscripts.js
+++ inger/studio/blog/postscripts.js
@@ -57,6 +57,260 @@ $(document).ready(function(){
       $img.hide();
   });
 
+  $('.gawd').mousemove(function(e) {
+        console.log("mousemove works");
+
+      var $img = $("#" + $(this).data('image-id'))
+      $img.show();
+      $img.offset({
+          top: e.pageY + 20,
+          left: e.pageX + 20
+      });
+  }).mouseleave(function() {
+      $img = $("#" + $(this).data('image-id'))
+      $img.hide();
+  });
+
+  $('.rupaul').mousemove(function(e) {
+        console.log("mousemove works");
+
+      var $img = $("#" + $(this).data('image-id'))
+      $img.show();
+      $img.offset({
+          top: e.pageY + 20,
+          left: e.pageX + 20
+      });
+  }).mouseleave(function() {
+      $img = $("#" + $(this).data('image-id'))
+      $img.hide();
+  });
+
+    $('.roxane').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.badfeminist').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.twelveyears').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.moonlight').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.beasts').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.roots').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.twelveyears').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.blindside').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.freedomwriters').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.precious').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.modernfamily').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.willandgrace').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.queerasfolk').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.transparent').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.hijras').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+    $('.baklas').mousemove(function(e) {
+          console.log("mousemove works");
+
+        var $img = $("#" + $(this).data('image-id'))
+        $img.show();
+        $img.offset({
+            top: e.pageY + 20,
+            left: e.pageX + 20
+        });
+    }).mouseleave(function() {
+        $img = $("#" + $(this).data('image-id'))
+        $img.hide();
+    });
+
+
+
 
 
   /*$(window).scroll(function() {
